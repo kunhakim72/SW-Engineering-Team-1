e3565df5a30d4e26ad117672b316086f818ceb85
Junepil Lee <hanu9257@gmail.com>
2024-11-25 17:15:58 +0900

    moveForward까지의 TC 구현

--- RVC_testcase.c
+++ RVC_testcase.c
@@ -1,11 +1,14 @@
 #include "RVC_software_engineering.h"
 
-void describe(char* name, int expected, int result) {
-  if (expected ==  result) {
-    printf("\033[1;32m[+] Test for %s success\033[0m\n", name);
+int testCount = 0;
+
+void describe(char *description, int expected, int result) {
+  ++testCount;
+  if (expected == result) {
+    printf("\033[1;32m[+] %d. Test for %s\033[0m\n", testCount, description);
     return;
   }
-  printf("\033[1;31m[-] Test for %s failed\033[0m\n", name);
+  printf("\033[1;31m[-] %d. Test for %s\033[0m\n", testCount, description);
   printf("\texpected: %d\n", expected);
   printf("\tresult: %d\n", result);
 }
@@ -15,25 +18,40 @@ void testTurnLeft() {
   // 1. power option = off
   describe("call power off interface when turn left", POWER_OFF, power);
   // 2. motion = turn left
-  describe("call turn left interface when turn left", TURN_LEFT, motion);
+  describe("call turn left interface when turn left", true,
+           callTurnLeftInterface);
+  resetTestCondition();
 }
 
-void testTurnRight(){
+void testTurnRight() {
+  turnRight();
   // 1. power option = off
+  describe("call power off interface when turn right", POWER_OFF, power);
   // 2. motion = turn right
+  describe("call turn right interface when turn right", true,
+           callTurnRightInterface);
+  resetTestCondition();
 }
 
-void testMoveBackward(){
+void testMoveBackward() {
+  moveBackward();
   // 1. power option = off
+  describe("call power off interface when move backward", POWER_OFF, power);
   // 2. motion = move backward
+  describe("call move backward interface when move backward", true,
+           callMoveBackwardInterface);
+  resetTestCondition();
 }
 
-void testMoveForward(){
+void testMoveForward() {
+  moveForward();
   // 1. power option = on
+  describe("call power on interface when move forward", POWER_ON, power);
   // 2. motion = move forward
+  describe("call move foward interface when move forward", true,
+           callMoveForwardInterface);
+  resetTestCondition();
 }
-
-// ---- 준필
 
 void testPowerUpAndMoveFoward() {
   // 1. power option = up
@@ -41,6 +59,7 @@ void testPowerUpAndMoveFoward() {
 }
 
 void testDetermineObstacleLocation() {
+  determineObstacleLocation();
   // 1. !L, !R, !F
   // 2. L, !R, !F
   // 3. !L, R, !F
@@ -52,6 +71,7 @@ void testDetermineObstacleLocation() {
 }
 
 void testDetermineDustExistence() {
+  determineDustExistence();
   // 1. D
   // 2. !D
 }
@@ -72,9 +92,6 @@ int main() {
   testDetermineDustExistence();
   testDetermineObstacleLocation();
   testController();
-      
+
   return 0;
 }
-
-
-
