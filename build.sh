bdea53727953a62333f1c272d288e999e220ba45
Junepil Lee <hanu9257@gmail.com>
2024-12-04 20:37:49 +0900

    feat: 빌드 스크립트 수정

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-gcc RVC_Cunit_test.c -o unit_test.out -I/opt/homebrew/include -L/opt/homebrew/lib -lcunit
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  gcc -o test_math_utils test_math_utils.c math_utils.c -lcunit
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  gcc RVC_Cunit_test.c -o unit_test.out -I/opt/homebrew/include -L/opt/homebrew/lib -lcunit
+elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+  echo "Windows are not supproted."
+else
+  echo "Unknown OS"
+fi
